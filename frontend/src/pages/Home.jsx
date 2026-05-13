bab23a7a6ae055cb921c4b77a931164980c3f064
Sachin chaudhary <sachinchaudhary9794@gmail.com>
2026-05-14 02:12:44 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import Navbar from '../Component/layout/Navbar'
+import Navbar from '../Component/layout/NavBar'
 import Footer from '../Component/layout/Footer'
 import HeroSection from '../Component/home/HeroSection'
 import FloatingSearch from '../Component/home/FloatingSearch'
