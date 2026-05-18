02e7405f2e6d90b455c1017d40ecba7ae4b9003f
sachinchaudhary1999 <sachinchaudhary9794@gmail.com>
2026-05-18 15:51:36 +0530

    feat:Fix the login and logout session issue

--- backend/config/token.js
+++ backend/config/token.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
 
 const genToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" })
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1d" })
 }
 
 export default genToken
